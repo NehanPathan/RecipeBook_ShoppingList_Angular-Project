794ed41bf656327a2d70fb0fda6fc5b41d4cfa0d
NehanPathan <pathannehan26@gmail.com>
2024-05-29 11:14:03 +0530

    Committing local changes before reverting

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpParams } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpParams
+} from '@angular/common/http';
 import { take, exhaustMap } from 'rxjs/operators';
 
 import { AuthService } from './auth.service';
